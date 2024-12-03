1e2a8f1e5e463d539f35f9af95b574f250ad4505
Hiranyabandi <105910673+Hiranyabandi@users.noreply.github.com>
2024-12-02 19:01:55 -0500

    modified

--- main.tf
+++ main.tf
@@ -30,6 +30,22 @@ resource "azurerm_resource_group" "rg" {
   tags = local.tags
 }
 
+// Virutal Network
+
+resource "azurerm_virtual_network" "venv" {
+  name                = "vnet-${var.class_name}${var.student_name}${var.environment}${random_integer.deployment_id_suffix.result}"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+}
+
+resource "azurerm_subnet" "subnet" {
+  name                 = "subnet-${var.class_name}${var.student_name}${var.environment}${random_integer.deployment_id_suffix.result}"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.venv.name
+  address_prefixes     = ["10.0.2.0/24"]
+  service_endpoints    = ["Microsoft.Sql", "Microsoft.Storage"]
+}
 
 // Storage Account
 
@@ -40,5 +56,48 @@ resource "azurerm_storage_account" "storage" {
   account_tier             = "Standard"
   account_replication_type = "LRS"
 
+  network_rules {
+    default_action             = "Deny"
+    ip_rules                   = ["100.0.0.1"]
+    virtual_network_subnet_ids = [azurerm_subnet.subnet.id]
+  }
   tags = local.tags
+}
+
+// SQL database and server
+
+resource "azurerm_mssql_server" "srv" {
+  name                         = "ssr-${var.class_name}-${var.student_name}-${var.environment}"
+  resource_group_name          = azurerm_resource_group.rg.name
+  location                     = azurerm_resource_group.rg.location
+  version                      = "12.0"
+  administrator_login          = "4dm1n157r470r"
+  administrator_login_password = "4-v3ry-53cr37-p455w0rd"
+}
+
+resource "azurerm_mssql_virtual_network_rule" "sql_venv_rule" {
+  name                                 = "vnet-rule-${var.class_name}${var.student_name}${var.environment}${random_integer.deployment_id_suffix.result}"
+  server_id                            = azurerm_mssql_server.srv.id
+  subnet_id                            = azurerm_subnet.subnet.id
+  ignore_missing_vnet_service_endpoint = false
+}
+
+
+resource "azurerm_mssql_database" "database" {
+  name         = "sqldb-${var.class_name}-${var.student_name}-${var.environment}"
+  server_id    = azurerm_mssql_server.srv.id
+  collation    = "SQL_Latin1_General_CP1_CI_AS"
+  license_type = "LicenseIncluded"
+  max_size_gb  = 2
+  sku_name     = "S0"
+  enclave_type = "VBS"
+
+  tags = {
+    foo = "bar"
+  }
+
+  # prevent the possibility of accidental data loss
+  lifecycle {
+    prevent_destroy = true
+  }
 }
