e9e7f47789274e2e5f92fdb665f3a441b5cf09c4
Hiranyabandi <105910673+Hiranyabandi@users.noreply.github.com>
2024-12-03 16:04:52 -0500

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -37,7 +37,7 @@ variable "environment" {
 variable "student_name" {
   description = "Application Name"
   type        = string
-  default     = "cford38"
+  default     = "hiranyabandi"
 }
 
 variable "class_name" {
